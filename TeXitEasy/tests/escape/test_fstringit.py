9736d18d6c029731d15db62d4cf308be91b6c85d
Christophe BAL <projetmbc@gmail.com>
2021-08-13 00:46:03 +0200

    TeXitEasy: tests use cbdevtools

--- TeXitEasy/tests/escape/test_fstringit.py
+++ TeXitEasy/tests/escape/test_fstringit.py
@@ -1,61 +1,30 @@
 #!/usr/bin/env python3
 
-from pathlib import Path
 from pytest import fixture
-import sys
 
-from orpyste.data import ReadBlock as READ
-
-
-# ----------------------- #
-# -- GENERAL CONSTANTS -- #
-# ----------------------- #
-
-THIS_DIR = Path(__file__).parent
-
-
-# ------------------------------------ #
-# -- MODULES IMPORTED FROM SOURCES! -- #
-# ------------------------------------ #
-
-PROJECT_NAME = 'TeXitEasy'
-MODULE_DIR   = THIS_DIR
-
-if not PROJECT_NAME in str(MODULE_DIR):
-    raise Exception(
-        "call the script from a working directory containing the project."
-    )
-
-while(not MODULE_DIR.name.startswith(PROJECT_NAME)):
-    MODULE_DIR = MODULE_DIR.parent
-
-
-sys.path.append(str(MODULE_DIR))
-
-from src.escape import fstringit
+from cbdevtools import *
 
 
 # ------------------------------------ #
 # -- FUNCTION(S) / CLASS(ES) TESTED -- #
 # ------------------------------------ #
 
-FSTRINGIT_FUNCTION = fstringit
+MODULE_DIR = addfindsrc(
+    file    = __file__,
+    project = 'TeXitEasy',
+)
+
+from src.escape import fstringit as FSTRINGIT_FUNCTION
 
 
 # ----------------------- #
 # -- DATAS FOR TESTING -- #
 # ----------------------- #
 
-WHAT_IS_TESTED = Path(__file__).stem
-WHAT_IS_TESTED = WHAT_IS_TESTED.replace('test_', '')
-
-THE_DATAS_FOR_TESTING = READ(
-    content = THIS_DIR / f'{WHAT_IS_TESTED}.peuf',
-    mode    = {"keyval:: =": ":default:"}
-)
+THE_DATAS_FOR_TESTING = build_datas_block(__file__)
 
 @fixture(scope="module")
-def or_datas(request):
+def orpyste_fix_block(request):
     THE_DATAS_FOR_TESTING.build()
 
     def remove_extras():
@@ -68,7 +37,7 @@ def or_datas(request):
 # -- GOOD FSTRINGIFY -- #
 # --------------------- #
 
-def test_latex_use_fstringit(or_datas):
+def test_latex_use_fstringit(orpyste_fix_block):
     tests = THE_DATAS_FOR_TESTING.mydict("std nosep nonb")
 
     for testname, infos in tests.items():
