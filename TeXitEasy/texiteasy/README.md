0620656c71c320c72480cf27f4b11cfc274b8242
Christophe BAL <projetmbc@gmail.com>
2021-08-24 17:12:20 +0200

    README OK

--- TeXitEasy/texiteasy/README.md
+++ TeXitEasy/texiteasy/README.md
@@ -13,9 +13,58 @@ About `TeXitEasy`
 This modules proposes some `Python` tools to work with `LaTeX`.
 
 
-<!-- :tutorial-START: -->
-<!-- :tutorial-END: -->
+Escaping
+--------
+
+### For `LaTeX`...
+
+The function `texiteasy.escape.escape` "escapes" special characters. Here is an example of use.
+
+~~~python
+from texiteasy.escape import escape, MODE_MATH
+
+txt = "\OH/ & ..."
+
+print("Default mode (MODE_TEXT):")
+print(escape(txt))
+print()
+print("Math mode:")
+print(escape(text = txt, mode = MODE_MATH))
+~~~
+
+Launched in a terminal, the preceding script prints the following output.
+
+~~~
+Default mode (MODE_TEXT):
+\textbackslash{}OH/ \& ...
+
+Math mode:
+\backslash{}OH/ \& ...
+~~~
 
 
-<!-- :version-START: -->
-<!-- :version-END: -->
+### For `Python`...
+
+Working together with `LaTeX` templates for f-strings is very boryng because of the curly braces. The fucntion `texiteasy.escape.fstringit` simplifies this as you can see in the following code. Note that `<:PYVAR_FOR_FSTRING:>` is used instead of ``{PYVAR_FOR_FSTRING}`` as this must normally be done in f-strings.
+
+~~~python
+from texiteasy.escape import fstringit
+
+texcode = r'''
+\NewDocumentCommand{ \fictivenv }
+                   { O{abc}m }{
+    \onemacro{<:PYVAR_FOR_FSTRING:>}{#2}
+}
+'''.strip()
+
+print(fstringit(texcode))
+~~~
+
+Launched in a terminal, the preceding script prints the following output.
+
+~~~
+\NewDocumentCommand{{ \fictivenv }}
+                   {{ O{{abc}}m }}{{
+    \onemacro{{{PYVAR_FOR_FSTRING}}}{{#2}}
+}}
+~~~
--- TeXitEasy/texiteasy/escape.py
+++ TeXitEasy/texiteasy/escape.py
@@ -44,31 +44,19 @@ PLACEHOLDERS_F_STRING = {
 
 ###
 # prototype::
-#     arg = ; // See Python typing...
-#           the text to be escaped.
-#     arg = _ in ["text" , "math"] ( MODE_TEXT ) ; // See Python typing...
-#           the ¨latex mode where the text will be used.
+#     text : the text to be escaped.
+#     mode : the ¨latex mode where the text will be used.
+#          @ :in: [MODE_MATH, MODE_TEXT]
 #
-#     :return: = ; // See Python typing...
-#                the text with all specific ¨latex characters escaped so as
-#                to be used verbatim in either a math formula or a text 
-#                regarding to the value of ``mode``.
-#
-# Here are two examples of use.
-#
-# term-python::
-#     >>> from texiteasy.escape import escape, MODE_MATH
-#     >>> txt = "\OH/ & ..."
-#     >>> print(escape(txt))
-#     \textbackslash{}OH/ \& ...
-#     >>> print(escape(text = txt, mode = MODE_MATH))
-#     \backslash{}OH/ \& ...
+#     :return: the text with all specific ¨latex characters escaped so as
+#              to be used verbatim in either a math formula or a text 
+#              regarding to the value of ``mode``.
 ###
 
 def escape(
     text: str,
     mode: str = MODE_TEXT
-):
+) -> str:
     if not mode in CHARS_TO_ESCAPE:
         raise ValueError("unknown mode.")
 
@@ -81,22 +69,19 @@ def escape(
     i    = 0
 
     while(i < imax):
+        if text[i] in toescape:
+            escaped_text += '\\' + text[i]
+            i            += 1
+            continue
+
         nothingfound = True
 
-        for onechar in toescape:
-            if text[i] == onechar:
-                escaped_text += '\\' + onechar
-                i            += 1
+        for onechar, latexcode in tolatexify:
+            if text[i:].startswith(onechar):
+                escaped_text += latexcode
+                i            += len(onechar)
                 nothingfound  = False
                 break
-
-        if nothingfound:
-            for onechar, latexcode in tolatexify:
-                if text[i:].startswith(onechar):
-                    escaped_text += latexcode
-                    i            += len(onechar)
-                    nothingfound  = False
-                    break
 
         if nothingfound:
             escaped_text += text[i]
@@ -111,33 +96,14 @@ def escape(
 
 ###
 # prototype::
-#     arg = ; // See Python typing...
-#           the ¨latex source to transform to a f-string.
+#     code : the ¨latex source to transform to a f-string template.
 #
-#     :return: = ; // See Python typing...
-#                the source with all specific ¨latex characters escaped so as
-#                to be used verbatim in either a math formula or a text 
-#                regarding to the value of ``mode``.
-#
-# Here is an example of use where latex::``<:PYVAR_FOR_FSTRING:>`` is used instead 
-# of ``{PYVAR_FOR_FSTRING}`` as this must be done for f-strings.
-#
-# term-python::
-#     >>> from escape import fstringit
-#     >>> texcode = r'''
-#     \NewDocumentCommand{ \fictivenv }
-#                     { O{abc}m }{
-#         \onemacro{<:PYVAR_FOR_FSTRING:>}{#2}
-#     }
-#     '''.strip()
-#     >>> print(fstringit(texcode))
-#     \NewDocumentCommand{{ \fictivenv }}
-#                     {{ O{{abc}}m }}{{
-#         \onemacro{{{PYVAR_FOR_FSTRING}}}{{#2}}
-#     }}
+#     :return: the source with all specific ¨latex characters escaped so as
+#              to be used verbatim in either a math formula or a text 
+#              regarding to the value of ``mode``.
 ###
 
-def fstringit(code: str):
+def fstringit(code: str) -> str:
     for brace in '{}':
         code = code.replace(brace, brace*2)
 
