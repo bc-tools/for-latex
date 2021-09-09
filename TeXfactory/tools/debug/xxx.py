43e4b4f04c8cab1b43bb59243022fa8ccd416019
Christophe BAL <projetmbc@gmail.com>
2021-09-09 09:31:34 +0200

    TeXfactory: preparing the Python src folder

---
+++ TeXfactory/tools/debug/xxx.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from cbdevtools import *
+
+
+# ------------------------------------ #
+# -- MODULES IMPORTED FROM SOURCES! -- #
+# ------------------------------------ #
+
+MODULE_DIR = addfindsrc(
+    file    = __file__,
+    project = 'TeXfactory',
+)
+
+
+# -------------- #
+# -- LET'S GO -- #
+# -------------- #
+
+from src import *
