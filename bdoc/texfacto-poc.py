9f30c74800e97cf6babebc4a5314c47f28c5919d
Christophe BAL <projetmbc@users.noreply.github.com>
2023-09-26 18:57:24 +0200

    bdoc [Minor changes in listing-input.tex]

--- bdoc/texfacto-poc.py
+++ bdoc/texfacto-poc.py
@@ -1,8 +1,12 @@
+from yaml import safe_load
+
 from src2prod import *
 
 PROJECT_DIR = Path(__file__).parent
+SOURCE_DIR  = PROJECT_DIR / Path('src')
 TARGET_DIR  = PROJECT_DIR.parent / PROJECT_DIR.name.lower()
 
+# Fichires souhaités
 project = Project(
     project = PROJECT_DIR,
     source  = Path('src'),
@@ -15,10 +19,27 @@ project = Project(
 project.build()
 
 allfiles = [f for f in project.lof]
-allfiles.sort()
+
+# Gestion de l'ordre
+
+
+def buildtoc(aboutfile):
+    with aboutfile.open(
+        encoding='utf8',
+        mode='r',
+    ) as f:
+        about_cfg = safe_load(f)
+
+    print(about_cfg['toc'])
+
+src_about = SOURCE_DIR / "about.yaml"
+
+if src_about in allfiles:
+    toc = buildtoc(src_about)
+
+# Gestion des sty
+
 
 for f in allfiles:
-    print(f.name)
-
-    # if f.suffix == ".yaml":
-    #     print(f)
+    if f.suffix == ".sty":
+        print(f.name)
