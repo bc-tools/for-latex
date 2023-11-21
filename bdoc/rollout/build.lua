cb79b33a5f6518122e2e0fb3c4be43ccec80e04b
Christophe BAL <projetmbc@users.noreply.github.com>
2023-11-21 23:47:05 +0100

    Ugly backup #18

--- bdoc/rollout/build.lua
+++ bdoc/rollout/build.lua
@@ -1,7 +1,9 @@
 module = "bdoc"
 
 sourcefiles = {"code/*.sty"}
-typesetfiles  = {"doc/*.tex"}
+
+typesetfiles     = {"doc/*.tex"}
+typesetsuppfiles = {"doc/*/*"}
 
 checkopts   = "-interaction=nonstopmode --shell-escape"
 -- checkopts   = "-interaction=batchmode --shell-escape"
