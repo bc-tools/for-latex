01e55932a127041c685c917869bce80d516f09f6
Christophe BAL <projetmbc@users.noreply.github.com>
2023-11-30 09:40:10 +0100

    Ugly backup.2 #2

--- bdoc/rollout/build.lua
+++ bdoc/rollout/build.lua
@@ -1,6 +1,8 @@
--- Source.
+-- Sources.
 --     * https://www.latex-project.org/news/2019/04/16/l3build-upload/
 --     * https://ctan.org/help/submit
+--     * https://ctan.org/license
+--     * https://ctan.org/topics/highscore
 
 module = "bdoc"
 
@@ -20,17 +22,17 @@ typesetopts = checkopts
 
 
 uploadconfig = {
-    author      = "Christophe BAL",
-    uploader    = "Christophe BAL",
-    email       = "projetmbc@gmail.com",
-    license     = "GPLv3",
-    pkg         = "bdoc",
-    version     = "1.0.0 [2023-11-29]",
-    update      = false,
-    summary     = 'This package proposes tools for writing "human friendly" documentations of LaTeX packages.',
-    topic       = {"doc", "dev"},
-    ctanPath    = "/macros/latex/contrib/bdoc",
-    repository  = "https://github.com/bc-tools/for-latex",
-    bugtracker  = "https://github.com/bc-tools/for-latex/issues",
-    note        = [[Uploaded automatically by l3build...]]
+    author     = "Christophe BAL",
+    uploader   = "Christophe BAL",
+    email      = "projetmbc@gmail.com",
+    license    = "gpl3",
+    pkg        = "bdoc",
+    version    = "1.0.0 [2023-11-29]",
+    update     = false,
+    summary    = 'This package proposes tools for writing "human friendly" documentations of LaTeX packages.',
+    topic      = {"doc-tool"},
+    ctanPath   = "/macros/latex/contrib/bdoc",
+    repository = "https://github.com/bc-tools/for-latex",
+    bugtracker = "https://github.com/bc-tools/for-latex/issues",
+    note       = [[Uploaded automatically by l3build...]]
 }
