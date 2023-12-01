f2c87d49e16caf3b31b7e1d94c11dd36b574834c
Christophe BAL <projetmbc@users.noreply.github.com>
2023-12-01 23:01:04 +0100

    tutodoc - CTAN upload [2nd try]

--- tutodoc/rollout/build.lua
+++ tutodoc/rollout/build.lua
@@ -23,17 +23,18 @@ typesetopts = checkopts
 
 
 uploadconfig = {
-    author     = "Christophe BAL",
-    uploader   = "Christophe BAL",
-    email      = "projetmbc@gmail.com",
-    license    = "gpl3",
-    pkg        = "tutodoc",
-    version    = "1.0.0 [2023-11-29]",
-    update     = false,
-    summary    = 'This package proposes tools for writing "human friendly" documentations of LaTeX packages.',
-    topic      = {"doc-tool"},
-    ctanPath   = "/macros/latex/contrib/tutodoc",
-    repository = "https://github.com/bc-tools/for-latex",
-    bugtracker = "https://github.com/bc-tools/for-latex/issues",
-    note       = [[Uploaded automatically by l3build...]]
+    version      = "1.0.0 [2023-11-29]",
+    announcement = "1st release: tutodoc can be used to type tutorial-like documentations.",
+    author       = "Christophe BAL",
+    uploader     = "Christophe BAL",
+    email        = "projetmbc@gmail.com",
+    license      = "gpl3",
+    pkg          = "tutodoc",
+    update       = false,
+    summary      = 'This package proposes tools for writing "human friendly" documentations of LaTeX packages.',
+    topic        = {"doc-tool"},
+    ctanPath     = "/macros/latex/contrib/tutodoc",
+    repository   = "https://github.com/bc-tools/for-latex",
+    bugtracker   = "https://github.com/bc-tools/for-latex/issues",
+    note         = [[Uploaded automatically by l3build...]]
 }
