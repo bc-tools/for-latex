7247de37c79a6c1bc66980c77899c93bfc4d10b5
Christophe BAL <projetmbc@users.noreply.github.com>
2023-12-10 21:33:26 +0100

    tutodoc - texfacto_POC: Contrib API localization - FIX needed

--- tutodoc/update-locale-contrib-poc.py
+++ tutodoc/update-locale-contrib-poc.py
@@ -105,8 +105,10 @@ for langdir in CONTRIB_DIR.glob("*"):
 
         print(f'    * Updating "{PROJECT_NAME}/src/{ctxt.name}".')
 
+        langname = all_status[langdir.stem].get("lang-api", langdir.stem)
+
         stycfgfile = SOURCE_DIR / ctxt.name
-        stycfgfile /= f"{PROJECT_NAME}-locale-{ctxt.stem}-{langdir.stem}.sty"
+        stycfgfile /= f"{PROJECT_NAME}-locale-{ctxt.stem}-{langname}.sty"
 
         texmacros = []
 
