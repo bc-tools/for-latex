1641edebe21010f604d0dcb45f99eae39bca6f11
projetmbc <projetmbc@gmail.com>
2024-09-29 13:55:08 +0200

    [texfacto] TeXfacto-ALPHA-DEV #1

--- TeXfacto-ALPHA-DEV/0-POC-0/tutodoc/texfacto_builder/__init__.py
+++
@@ -1,7 +0,0 @@
-from .autoreplace import *
-from .finalize    import *
-from .lof         import *
-from .meta        import *
-from .misc        import *
-from .sty_pack    import *
-from .tex_doc     import *
