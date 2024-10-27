a97e9a42efd5068f3056e513610e2d801ec6a21f
projetmbc <projetmbc@gmail.com>
2024-10-28 00:21:16 +0100

    [tutodoc] TeXfacto is needed

---
+++ TeXfacto-ALPHA-DEV/0-POC-0/tutodoc-CLS/texfacto_builder/__init__.py
@@ -0,0 +1,9 @@
+from .autoreplace import *
+from .finalize    import *
+from .lof         import *
+from .meta        import *
+from .misc        import *
+
+from .cls_proj import *
+from .sty_proj import *
+from .tex_doc  import *
