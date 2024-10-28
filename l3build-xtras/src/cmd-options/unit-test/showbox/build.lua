30c2c7fb5eafe427a546bf1614f0759c5f756ab7
projetmbc <projetmbc@gmail.com>
2024-10-28 13:01:34 +0100

    [l3build_xtras] Preversion #2

---
+++ l3build-xtras/src/cmd-options/unit-test/showbox/build.lua
@@ -0,0 +1,309 @@
+---------------------------
+-- REOP. COMMON SETTINGS --
+---------------------------
+
+module = "l3build-xtras"
+
+sourcefiledir = "code"
+sourcefiles   = {"*.cls", "*.sty"}
+
+docfiledir   = "doc"
+typesetfiles = {"*.tex"}
+
+testfiledir = "tests"
+
+checkopts   = "-interaction=nonstopmode --shell-escape"
+typesetopts = checkopts
+
+
+---------------
+-- DIFF TOOL --
+---------------
+
+DIFF_OPTION_SIDE_BY_SIDE  = "-y"
+DIFF_OPTION_FULL_ONE_PAGE = "-u"
+
+
+function diff_files(
+    initial_name, initial_content,
+    final_name  , final_content,
+    options
+)
+    local initial_file = io.open(initial_name, "w")
+
+    if initial_file == nil
+    then
+        return raiseFileNotCreated(initial_name)
+    end
+
+    local final_file = io.open(final_name, "w")
+
+    if final_file == nil
+    then
+        os.remove(initial_file)
+
+        return raiseFileNotCreated(final_name)
+    end
+
+    initial_file:write(initial_content)
+    final_file:write(final_content)
+
+    local iostream = assert(
+        io.popen(
+            "diff "
+            -- .. DIFF_OPTION_SIDE_BY_SIDE ..
+            .. DIFF_OPTION_FULL_ONE_PAGE ..
+            " "
+            .. initial_name ..
+            " "
+            .. final_name,
+            'r'
+        )
+    )
+
+    local infos = assert(iostream:read('a'))
+
+    iostream:close()
+
+    infos = string.gsub(infos, '^%s+', '')
+    infos = string.gsub(infos, '%s+$', '')
+
+    print("`diff -u test check` gives:")
+    print("")
+
+    print(infos)
+
+    os.remove(initial_name)
+    os.remove(final_name)
+
+    return false
+end
+
+
+--------------------
+-- RAISING ERRORS --
+--------------------
+
+function raise(kind, message)
+    print("")
+    print(kind .. ".ERROR! " .. message)
+
+    return 1
+end
+
+function raiseFileNotCreated(file_name)
+    return raise(
+        "IO",
+        "File ''" .. file_name .. "'' can't be created."
+    )
+end
+
+
+-------------------
+-- CMD UTILITIES --
+-------------------
+
+function helper(
+    xtra_args,
+    text_helper
+)
+    if (
+        #xtra_args == 1
+        and
+        (
+            xtra_args[1] == "@"
+            or
+            xtra_args[1] == "@@bout"
+        )
+    )
+    then
+        print(text_helper)
+        return 0
+    end
+
+    return nil
+end
+
+
+--------------------------------
+-- L3BUILD EXTRA CMD "sbunit" --
+--------------------------------
+
+----
+-- This extra unit-testing command validates [s]how[b]ox comparisons?
+--
+-- Source.
+--    + l3build use of \showbox: https://tex.stackexchange.com/a/729463/6880
+----
+
+SBUNIT_HELPER = [[
+l3build extra command "sbunit"
+
+    GGGG
+]]
+
+cmd_option = "sbunit"
+
+TAG_CHECK  = "check"
+TAG_IGNORE = "standard"
+TAG_TEST   = "test"
+
+TAG_L3BUILD_BOX_START = "> \\box"
+TAG_L3BUILD_BOX_END   = "! OK."
+
+function checkSBUNIT(xtra_args)
+    if (
+        xtra_args == nil
+        or (
+            #xtra_args == 0
+            or
+            #xtra_args > 1
+        )
+    )
+    then
+        return raise(
+            "IO",
+            "One single test file name needed!"
+        )
+    end
+
+    -- local help =
+
+    if helper(
+        xtra_args,
+        SBUNIT_HELPER
+    ) ~= nil
+    then
+        return 0
+    end
+
+    print("SHOWBOX COMPARISON validated?")
+
+    local testfilename = xtra_args[1]
+    local logfile      = testfilename .. ".log"
+
+    local file  = io.open(testdir .. "/" .. logfile)
+    local lines = file:lines()
+
+    local nbline = 0
+
+    local nbtests = 0
+    local kind    = TAG_IGNORE
+
+    local line_test = -1
+    local log_test  = ""
+
+    local line_check = -1
+    local log_check  = ""
+
+    for line in lines
+    do
+        nbline    = nbline + 1
+        startline = string.sub(line, 1, 9)
+
+        if startline == "SB-TESTED"
+        then
+            line_test = nbline
+            kind      = TAG_TEST
+            catego    = string.sub(line, 11, -1)
+
+        elseif startline == "SB-WANTED"
+        then
+            if catego ~= string.sub(line, 11, -1)
+            then
+                return raise(
+                    "FORMAT",
+                    "Showbox checker without a showbox tested: see line "
+                    .. nbline ..
+                    " in the log file. Tested? Same category"
+                )
+            end
+
+
+            line_check = nbline
+            kind       = TAG_CHECK
+
+        elseif line == TAG_L3BUILD_BOX_END
+        then
+            if kind == TAG_CHECK
+            then
+                error_code = _compareSBUNIT(
+                    catego,
+                    line_test , log_test,
+                    line_check, log_check
+                )
+
+                if error_code == 1
+                then
+                    return error_code
+                end
+
+                if error_code == false
+                then
+                    return raise(
+                        "TEST",
+                        "Validation has failed!"
+                    )
+                end
+            end
+
+            kind = TAG_IGNORE
+
+        elseif string.sub(line, 0, 6) ~= TAG_L3BUILD_BOX_START
+        then
+            if kind == TAG_TEST
+            then
+                log_test = log_test .. "\n" .. line
+
+            elseif kind == TAG_CHECK
+            then
+                log_check = log_check .. "\n" .. line
+            end
+        end
+    end
+
+    print("SHOWBOX COMPARISON: OK!")
+
+    return 0
+end
+
+
+target_list[cmd_option] = {
+    func = checkSBUNIT,
+    desc = "Check showbox comparison unit test",
+--   pre = function(xtra_args)
+--   end
+}
+
+
+function _compareSBUNIT(
+    catego,
+    line_test , log_test,
+    line_check, log_check
+)
+    local what   = "Showbox " .. catego
+
+-- Happy day!
+    if log_test == log_check
+    then
+        result = true
+
+        print("    + [OK] " .. what)
+
+        return true
+    end
+
+-- Chaos day...
+    print("    + [KO] " .. what)
+
+    print("LOG FILE. Test starts at line " .. line_test .. ".")
+    print("          Check starts at line " .. line_check .. ".")
+
+    diff_files(
+        "build/test/" .. "@_@test_@_l3build@_@",
+        log_test,
+        "build/test/" .."@_@check_@_l3build@_@",
+        log_check,
+        nil
+    )
+end
---
+++ l3build-xtras/src/python/paralell-processing/checktests.py
@@ -0,0 +1,63 @@
+# Source: https://stackoverflow.com/a/25120960/4589608
+
+from pathlib import Path
+from random  import shuffle
+
+THIS_DIR = Path(__file__).parent
+
+TESTS_DIR = THIS_DIR / "tests"
+
+testfiles = [
+    p.stem
+    for p in TESTS_DIR.glob("*.lvt")
+]
+
+shuffle(testfiles)
+
+import multiprocessing
+import subprocess
+import shlex
+
+from multiprocessing.pool import ThreadPool
+
+
+def call_proc(cmd):
+    """ This runs in a separate thread. """
+    #subprocess.call(shlex.split(cmd))  # This will block until cmd finishes
+    print(cmd)
+
+    p = subprocess.Popen(
+        shlex.split(cmd),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+
+    out, err = p.communicate()
+
+    return (out, err)
+
+
+pool = ThreadPool(multiprocessing.cpu_count())
+
+results = []
+
+for tname in testfiles:
+    results.append(
+        (
+            tname,
+                pool.apply_async(
+                call_proc,
+                (f"l3build check {tname}",)
+            )
+        )
+    )
+
+# Close the pool and wait for each running task to complete
+pool.close()
+pool.join()
+
+for tname, result in results:
+    out, err = result.get()
+
+    if b"--> failed" in out:
+        print(f"   + [KO] {tname}")
