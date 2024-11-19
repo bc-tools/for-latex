582eea54132e6d3c718c179317e01461e07ec642
projetmbc <projetmbc@gmail.com>
2024-11-20 00:18:20 +0100

    [texfacto] TeXfacto as a new project #9

---
+++ TeXfacto/src/metaimp.py
@@ -0,0 +1,44 @@
+import re
+
+test_str = r"""
+\RequirePackage[
+  top            = 2.5cm,
+  bottom         = 2.5cm,
+  left           = 2.5cm,
+  right          = 2.5cm,
+  marginparwidth = 2cm,
+  marginparsep   = 2mm,
+  heightrounded
+]{geometry}%
+ [2020-01-02]
+
+\RequirePackage{tocbasic}
+
+\RequirePackage[svgnames]%
+               {xcolor}%
+               [2023/11/15]
+
+\RequirePackage[raggedright]%  OK?
+                {titlesec}
+
+\RequirePackage{xcolor}%
+               [2022/06/12]
+
+\RequirePackage{hyperref}% To load after titlesec!
+               [2023-02-07]
+    """
+
+pattern = re.compile(
+    r"\\RequirePackage(\[(.*?)\])?([^{]*?)?{(.*?)}",
+    flags = re.S
+)
+
+matches = pattern.finditer(test_str)
+
+for m in matches:
+    print('---')
+
+    for i in [0, 2, 4]:
+        print(f"m.group({i}):")
+        print(m.group(i))
+        print()
