104ad31f6337da06fb8d18c3359b1302b188413d
projetmbc <projetmbc@gmail.com>
2024-11-30 12:14:29 +0100

    [tutodoc] Preparing new version #34 - TeXdepends creation

---
+++ TeXdepends/readme/about.md
@@ -0,0 +1,4 @@
+About `TeXfacto`
+----------------
+
+This module proposes tools to automate the production of a `TeX` project from a `STY` and/or `CLS` source directory using a standardized workflow (the idea is to code using small parts of the project rather than big files).
--- TeXdepends/src/metaimp.py
+++ TeXdepends/src/metaimp.py
@@ -12,145 +12,10 @@ import re
 PATH_FILE = Path(__file__).parent.parent.parent / "tutodoc" / "src" / "main" / "main.cls"
 YAML_FILE = Path(__file__).parent.parent.parent / "tutodoc" / "src" / "DEPENDS.yaml"
 
-test_str = r"""
-% ********************************************************* %
-% ** This is file `tutodoc.cls' generated automatically. ** %
-% **                                                     ** %
-% ** Copyright (C) 2023-2024 by Christophe, BAL          ** %
-% **                                                     ** %
-% ** This file may be distributed and/or modified under  ** %
-% ** the conditions of the GNU 3 License.                ** %
-% ********************************************************* %
-
-\ProvidesExplClass
-  {tutodoc}
-  {2024-10-30}  % Creation: 2023-11-29
-  {1.6.2}
-  {This package proposes tools for writing "human friendly" documentations of LaTeX packages.}
-
-
-% =================== %
-% == PACKAGES USED == %
-% =================== %
-
-% ------------- %
-% -- OPTIONS -- %
-% ------------- %
-
-\ExplSyntaxOn
-
-\tl_new:N \l__tutodoc_other_cls_options_tl
-
-\newcommand{ \tutodoc@theme } { color }
-
-\keys_define:nn { tutodoc / main / class / options } {
-  theme .choices:nn = {
-    bw,
-    color,
-    dark,
-    draft
-  } {
-    \renewcommand{ \tutodoc@theme } {
-      \tl_use:N \l_keys_choice_tl
-    }
-  },
-  unknown .code:n = {
-    \tl_set:Nn \l__tutodoc_other_cls_options_tl { #1 }
-  }
-}
-
-\ProcessKeyOptions[tutodoc / main / class / options]
-
-\exp_last_unbraced:NNV \LoadClass[ \l__tutodoc_other_cls_options_tl ]%
-          { article }%
-          [ 2022-06-01 ]
-
-\ExplSyntaxOff
-
-
-% ------------- %
-% -- IMPORTS -- %
-% ------------- %
-
-\RequirePackage[svgnames]%
-               {xcolor}%
-               [2023/11/15]
-
-\RequirePackage[
-  top            = 2.5cm,
-  bottom         = 2.5cm,
-  left           = 2.5cm,
-  right          = 2.5cm,
-  marginparwidth = 2cm,
-  marginparsep   = 2mm,
-  heightrounded
-]{geometry}%
- [2020-01-02]
-
-\RequirePackage[raggedright]%
-               {titlesec}%
-               [2023/10/27]
-
-\RequirePackage{tocbasic}
-
-\RequirePackage{xcolor}%
-               [2022/06/12]
-
-\RequirePackage{hyperref}% To load after titlesec!
-               [2023-02-07]
-
-
-% We delegate the management of quoting to the ''csquotes'' package,
-% which takes care of the locale settings.
-\RequirePackage{csquotes}%
-               [2022-09-14]
-
-
-\RequirePackage{fontawesome5}%
-               [2022-05-02]
-
-\RequirePackage{keytheorems}%
-               [2024/09/19]
-
-\RequirePackage[svgnames]%
-               {xcolor}%
-               [2023/11/15]
-
-\RequirePackage{tcolorbox}%
-               [2024/07/10]
-
-\tcbuselibrary{breakable,
-               skins}
-
-
-\RequirePackage{clrstrip}%
-               [2021-08-28]
-
-
-\RequirePackage[highlightmode = immediate]%
-               {minted}%
-               [2024/09/22]
-
-\RequirePackage{tcolorbox}%
-               [2024/07/10]
-
-\tcbuselibrary{minted,
-               breakable,
-               skins}
-
-
-\RequirePackage{fontawesome5}%
-               [2022-05-02]
-
-\RequirePackage{marginnote}%
-               [2018/08/09]
-
-
-    """
 
 pack_require_like_pattern = re.compile(
     r"""
-        \\(?P<kind>RequirePackage|LoadClass|tcbuselibrary) #
+        \\(?P<kind>RequirePackage|LoadClass|tcbuselibrary|geometry) #
         (\[(?P<options>[^][]*)\])?              #
         (?P<xtra_1>[^{]*)                      #
         {(?P<name>[^{}]*)}                   #
@@ -195,6 +60,14 @@ specific_actions = {
     "geometry": set(),
 }
 
+
+def clean_options(option):
+    option = option.split("=")
+    option = [x.strip() for x in option]
+    option = " = ".join(option)
+    return option
+
+
 for m in matches:
     kind = m["kind"].strip()
 
@@ -202,7 +75,7 @@ for m in matches:
     if kind in specific_actions:
         specific_actions[kind] = specific_actions[kind].union(
             set(
-                x.strip()
+                clean_options(x)
                 for x in m["name"].split(',')
             )
         )
@@ -227,7 +100,11 @@ for m in matches:
 
     for i in ["options", "date"]:
         if not m[i] is None:
-            thismeta[i].add(m[i])
+            thismeta[i].add(
+                m[i]
+                if i == "options" else
+                clean_options(m[i])
+            )
 
     metaimports[kind][name] = deepcopy(thismeta)
 
@@ -243,7 +120,7 @@ for m in matches:
     if not name in metaimports["LoadClass"]:
         raise ValueError(f"the class '{name}' has not been loaded!")
 
-    options = m["options"].strip()
+    options = clean_options(m["options"])
 
     metaimports["LoadClass"][name]["options"].add(options)
 
@@ -260,8 +137,6 @@ yaml_storing = {}
 
 for kind, texnames in alltexnames.items():
     thiskind = []
-
-    print(f"-- {kind}")
 
     for tname in texnames:
         for what, metawhat in metaimports[tname].items():
@@ -294,6 +169,17 @@ for kind, texnames in alltexnames.items():
 print(yaml_storing)
 
 
+
+YAML_FILE.touch()
+
+with YAML_FILE.open(
+    mode = 'w',
+) as f:
+    dump(yaml_storing, f)
+
+
+
+
 print(f"\n{specific_actions=}")
 
 exit()
@@ -308,12 +194,3 @@ print(metaimports)
 exit()
 
 yaml_storing = {}
-
-
-
-YAML_FILE.touch()
-
-with YAML_FILE.open(
-    mode = 'w',
-) as f:
-    dump(metaimports, f)
