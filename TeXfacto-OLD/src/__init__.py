b17756f60a12f506b90559a42a95a4deb5122a56
projetmbc <projetmbc@gmail.com>
2024-12-04 22:37:41 +0100

    [tutodoc] New version 1.7.0 [in main branch]

--- TeXfacto-OLD/src/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-from typing import *
-
-from .buildproj  import *
-from .texproject import *
-from .utils      import *
--- TeXoptidep/src/__init__.py
+++
@@ -1,4 +0,0 @@
-from .lazyextract import *
-from .fulldep     import *
-
-from .dump import *
