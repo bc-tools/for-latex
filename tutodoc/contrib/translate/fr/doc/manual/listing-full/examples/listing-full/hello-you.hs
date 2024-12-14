6a7035bdcded28bd65a0a5b4c1d9d4c308c4bc79
projetmbc <projetmbc@gmail.com>
2024-12-14 14:25:54 +0100

    [tutodoc] Any listing #87

---
+++ tutodoc/contrib/translate/fr/doc/manual/listing-full/examples/listing-full/hello-you.hs
@@ -0,0 +1,12 @@
+main :: IO ()
+
+main = do
+    putStr "Qui êtes-vous ? "
+    name <- getLine
+
+    if name == ""
+        then putStrLn "Ah, pas très bavard aujourd'hui !"
+
+    else do
+        putStrLn ("Bonjour " ++ name ++ ".")
+        putStrLn "Épatant ! En fait, pas du tout..."
