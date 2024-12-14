42285bb3154c8a12cb0b4fcf4489570882c65534
projetmbc <projetmbc@gmail.com>
2024-12-14 14:23:27 +0100

    [tutodoc] Any listing #86

--- tutodoc/contrib/translate/en/doc/manual/listing-full/examples/listing-full/hello-you.py
+++
@@ -1,8 +0,0 @@
-name = input("Qui êtes-vous ?")
-
-if not name:
-    print("Ah, pas très bavard aujourd'hui !")
-
-else:
-    print(f"Bonjour {name}.")
-    print("Épatant ! En fait, pas du tout...")
